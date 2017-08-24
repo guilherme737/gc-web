4c17390d805aa82282c75893501495fe34e9569e
Guilherme Mendes <guilherme.mendes@megasoftgyn.com.br>
2017-08-24 17:21:00 -0300

    Add Parent in routes

--- app/membro/membro.routes.js
+++ app/membro/membro.routes.js
@@ -8,12 +8,14 @@ function MembroRoute($stateProvider) {
 
     $stateProvider.state('membro-novo',{
         url: '/membro/novo',
+        parent:'home',
         controller: 'MembroFormController',
         templateUrl: 'membro/membro.form.html',
         authorize:true
 
     }).state('membro-editar', {
         url: '/membro/:id',
+        parent:'home',
         controller: 'MembroFormController',
         templateUrl: 'membro/membro.form.html',
         authorize:true
